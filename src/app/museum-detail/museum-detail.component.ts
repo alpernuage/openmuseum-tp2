61ceea8bcd97ebccf4d1f784169dfac11ef77e3c
Alper <alperakbulut@gmail.com>
2020-04-22 12:35:48 +0200

    add all variables

--- src/app/museum-detail/museum-detail.component.ts
+++ src/app/museum-detail/museum-detail.component.ts
@@ -16,13 +16,20 @@ export class MuseumDetailComponent implements OnInit {
   museumPhone: string;
   museumWebSite: string;
   museumOpening: string;
+  museumFax: string;
 
   constructor(private museumListService: MuseumListService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     const museumRef = this.route.snapshot.params['museumRef'];
     this.museumName = this.museumListService.getMuseumByRefMusee(museumRef).nom_du_musee;
+    this.museumAdress = this.museumListService.getMuseumByOpening(museumRef).adr;
+    this.museumZipCode = this.museumListService.getMuseumByRefMusee(museumRef).cp;
+    this.museumCity = this.museumListService.getMuseumByOpening(museumRef).ville;
+    this.museumPhone = this.museumListService.getMuseumByRefMusee(museumRef).telephone;
+    this.museumWebSite = this.museumListService.getMuseumByOpening(museumRef).siteweb;
     this.museumOpening = this.museumListService.getMuseumByOpening(museumRef).periode_ouverture;
+    this.museumFax = this.museumListService.getMuseumByOpening(museumRef).fax;
   }
 
 }
